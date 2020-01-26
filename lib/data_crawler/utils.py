b717ddc4b6b841fd457b13bae85c30f363bd68b8
bozcani <ilkerbozcan@hotmail.com>
2020-01-26 20:17:58 +0100

    Initial steps

---
+++ lib/data_crawler/utils.py
@@ -0,0 +1,30 @@
+import urllib.request as urllib
+from bs4 import BeautifulSoup
+
+
+def get_bist_tickers(link_to_source):
+
+    #r = requests.get(link_to_source, "bist-ticker-names.xls")
+    #a = urllib.urlretrieve(link_to_source)
+    #print(a)
+    a = ("tmpp285ccnk",3)
+    a = open(a[0], "r")
+    soup = BeautifulSoup(a, 'html.parser')
+    #print(soup.prettify())
+    items = soup.findAll("td", {"class":"comp-cell _04 vtable"})
+    names = soup.findAll("td", {"class":"comp-cell _14 vtable"})
+
+    res = []
+    for item,name in zip(items,names):
+        
+        tickers = item.find("a", {'class': "vcell"}).text.split(",")
+        tickers = [ticker.strip() for ticker in tickers]
+        
+        name = name.find("a", {'class': "vcell"}).text.split(",")
+        name = ''.join(str(elem) for elem in name)
+
+        link = item.find("a",href=True).get("href")
+
+        res.append([tickers, name, link])
+
+    return res
