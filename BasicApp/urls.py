cb03e27290e9907ec28d8a24073922b6fce88bdf
bozcani <ilkerbozcan@hotmail.com>
2020-02-01 18:09:11 +0100

    Fix template dir

--- BasicApp/urls.py
+++ BasicApp/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 
 from . import views
 
+
+app_name = 'BasicApp'
 urlpatterns = [
     path('', views.home, name='home'),
     path('admin/', admin.site.urls),
--- BasicApp/views.py
+++ BasicApp/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-
+from django.template import loader
 
 def home(request):
-    return HttpResponse("Hello, world. This is BasicApp index.")
 
-    template = loader.get_template('gezi/index.html')
-
-    return HttpResponse(template.render(request))    
+    template = loader.get_template('home.html')
+    context = {}
+    return HttpResponse(template.render(context, request))    
