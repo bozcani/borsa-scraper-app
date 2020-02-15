4b7323e6f3fe887f371a7d846b14f1e9288c6a7b
bozcani <ilkerbozcan@hotmail.com>
2020-02-15 14:43:25 +0100

    Remove updatestatuslookuptable

--- BasicApp/views.py
+++ BasicApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 from django.http import HttpResponse
 from django.template import loader
-from .models import StockMarket
+from .models import StockMarket, LookupTablesUpdateStatus
 
 from lib.data_scraper.get_stock_markets_info import create_stock_market_tables_from_wikipedia
 import json
@@ -75,23 +75,19 @@ def update_stock_market_lookup_table(request):
     num_samples = len(table_data['name'])
 
     for i in range(num_samples):
-        new_market = StockMarket(market_id=table_data['id'][i], 
-                    market_name=table_data['name'][i], 
-                    country=table_data['country'][i],
-                    city=table_data['city'][i],
-                    time_zone=table_data['sort'][i],
-                    open_time=table_data['open'][i],
-                    close_time=table_data['close'][i],
-                    lunch_break=table_data['lunch'][i])
-        new_market.save()
 
-    return redirect("/BasicApp/data_manager")    
+        if not StockMarket.objects.filter(market_id=table_data['id'][i]).exists():
 
-def create_LookupTablesUpdateStatus_table(request):
-    #TODO implement this
-    stock_markets = StockMarket.objects.all()
-
-    template = loader.get_template('data_manager.html')
-    context = {'stock_markets': stock_markets}
+            new_market = StockMarket(market_id=table_data['id'][i], 
+                        market_name=table_data['name'][i], 
+                        country=table_data['country'][i],
+                        city=table_data['city'][i],
+                        time_zone=table_data['sort'][i],
+                        open_time=table_data['open'][i],
+                        close_time=table_data['close'][i],
+                        lunch_break=table_data['lunch'][i])
+            new_market.save()
+        else:
+            print(StockMarket.objects.get(market_id=table_data['id'][i]), " exists.")
 
     return redirect("/BasicApp/data_manager")
