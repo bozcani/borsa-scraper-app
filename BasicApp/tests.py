3fd8a9a87ca94a366f241d400dec1c16274b9e38
bozcani <ilkerbozcan@hotmail.com>
2020-03-08 22:55:51 +0100

    Add unittests for data_manager view

--- BasicApp/tests.py
+++ BasicApp/tests.py
@@ -1,7 +1,7 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 from django.urls import reverse, resolve
-from .views import home, stock_market, delete_stock_market
+from .views import home, stock_market, delete_stock_market, data_manager
 from .models import StockMarket
 
 
@@ -31,9 +31,11 @@ class HomeTests(TestCase):
         stock_market_url = reverse('BasicApp:stock_market', kwargs={'market_id': 'TEST'})
         self.assertContains(self.response, 'a href="{}"'.format(stock_market_url[10:])) # Remove "/BasicApp/" from leading.
 
+    """
     def test_home_view_contains_link_to_StockMarketDelete_page(self):
         stock_market_url = reverse('BasicApp:delete_stock_market', kwargs={'market_id': 'TEST'})
         self.assertContains(self.response, 'a href="{}"'.format(stock_market_url[10:])) # Remove "/BasicApp/" from leading.
+    """
 
     def test_new_StockMarket_post_data_success_status_code(self):
         url = reverse('BasicApp:home')
@@ -102,3 +104,62 @@ class AddStockMarket(TestCase):
         url = reverse('BasicApp:add_stock_market_result')
         response = self.client.get(url, self.form_data)
         self.assertEquals(response.status_code, 302)    
+
+
+class DataManagerTests(TestCase):
+    def setUp(self):
+        StockMarket.objects.create(market_id='TEST', 
+                                    market_name='Test stock market',
+                                    country='Turkey',
+                                    city='Buharkent',
+                                    time_zone=3,
+                                    open_time='10:00',
+                                    close_time='18:00',
+                                    lunch_break='13:00-14:00')
+
+    
+    def test_DataManager_view_success_status_code(self):
+        url = reverse('BasicApp:data_manager')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_DataManager_url_resolves_new_topic_view(self):
+        view = resolve('/BasicApp/data_manager/')
+        self.assertEquals(view.func, data_manager)   
+
+    def test_UpdateStockMarketsLookupTable_button_exist(self):
+        url = reverse('BasicApp:data_manager')
+        response = self.client.get(url)
+        button_link = reverse('BasicApp:update_stock_market_lookup_table')
+        self.assertContains(response, "action=\"{}\"".format(button_link))
+
+    def test_UpdateStockLookupTable_button_exist(self):
+        url = reverse('BasicApp:data_manager')
+        response = self.client.get(url)
+        button_link = reverse('BasicApp:update_stock_lookup_table')
+        self.assertContains(response, "action=\"{}\"".format(button_link))
+
+    
+    def test_DeleteStockMarketsLookupTable_button_exist(self):
+        url = reverse('BasicApp:data_manager')
+        response = self.client.get(url)
+        button_link = reverse('BasicApp:delete_stock_market_lookup_table')
+        self.assertContains(response, "action=\"{}\"".format(button_link))
+
+    def test_UpdateCookieCrumbPair_button_exist(self):
+        url = reverse('BasicApp:data_manager')
+        response = self.client.get(url)
+        button_link = reverse('BasicApp:update_cookie_crumb_pair')
+        self.assertContains(response, "action=\"{}\"".format(button_link))
+
+    def test_view_contains_link_back_to_homepage(self):
+        url = reverse('BasicApp:data_manager')
+        response = self.client.get(url)
+        homepage_url = reverse('BasicApp:home')
+        self.assertContains(response, 'href="{}"'.format(homepage_url))     
+
+    def test_drop_down_menu_exist(self):
+        url = reverse('BasicApp:data_manager')
+        response = self.client.get(url)
+        homepage_url = reverse('BasicApp:home')
+        self.assertContains(response, 'option value=\"TEST\"')     
